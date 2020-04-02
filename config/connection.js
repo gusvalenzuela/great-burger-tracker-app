02eaef6fa7f22593fa41648a1bfd532fbc944846
Gus (Nala) <gusrosales7@gmail.com>
2020-04-02 16:26:09 -0700

    changes to connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,23 +3,28 @@
 // *********************************************************************************
 
 // Dependencies
-var mysql = require(`mysql`)
+const mysql = require(`mysql`)
+let connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: `localhost`,
+    user: `root`,
+    password: `r00t`,
+    database: `burgers_db`
+  })
 
-var connection = mysql.createConnection({
-  host: `localhost`,
-  port: 3306,
-  user: `root`,
-  password: `r00t`,
-  database: `burgers_db`
-})
+}
+
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error(`error connecting: ` + err.stack)
     return
   }
-  console.log(` > connected as id ` + connection.threadId + ` to database "` + connection.config.database + `"` )
+  console.log(` > connected as id ` + connection.threadId + ` to database "` + connection.config.database + `"`)
 })
 
 // Export connection for our ORM to use.
